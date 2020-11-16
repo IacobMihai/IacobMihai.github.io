e3b69f7102d1d573f71848850a637186eb76c196
ancagiurgiu <anca_g3595@yahoo.com>
2020-11-16 21:03:27 +0200

    js

--- functions.js
+++ functions.js
@@ -44,7 +44,6 @@ var skills = ["HTML", "CSS", "JS"];
 // TODO add "favorite skill"
 
 var ul = document.querySelector("#skills ul");
-ul.innerHTML = 
-"<li>" + skills[0] + "</li>" +
-"<li>" + skills[1] + "</li>" +
-"<li>" + skills[2] + "</li>";
+ul.innerHTML =  "<li>" + skills[0] + "</li>" +
+                "<li>" + skills[1] + "</li>" +
+                "<li>" + skills[2] + "</li>";
